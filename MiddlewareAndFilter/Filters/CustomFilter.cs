0d21d4be9ee4ab8a82a3c3b3c400c7583d299d23
Daniel Avila Avila <avila.daniel86@gmail.com>
2021-07-22 14:10:33 -0500

    Add project files.

---
+++ MiddlewareAndFilter/Filters/CustomFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareAndFilter.Filters
+{
+    public class CustomFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine("OnActionExecuting from Filter");
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine("OnActionExecuted from filter");
+        }
+    }
+}
---
+++ MiddlewareAndFilter/Middlewares/CustomLoggerMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareAndFilter.Middlewares
+{
+    public class CustomLoggerMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            Console.WriteLine("Call before request Middleware");
+            await next(context);
+            Console.WriteLine("Call after request Middleware");
+        }
+    }
+}
